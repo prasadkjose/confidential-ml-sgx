514fd2662b53a8290a72a1ff44a7bec77e583922
prasadkjose <prasadkjose@gmail.com>
2020-04-13 00:16:26 +0200

    Encrypter CLI init and added gitignore

---
+++ Encrypter/EncrypterCLI/EncrypterCLI/EncrypterCLI.h
@@ -0,0 +1,12 @@
+#pragma once
+//----------------------------------------------------------
+// This seems to be the standard solution for preventing
+// spurious "macro redefinition" warnings when you have
+// to include both windows.h and ntstatus.h
+#define WIN32_NO_STATUS
+#include <windows.h>
+#undef WIN32_NO_STATUS
+#include <ntstatus.h>
+//----------------------------------------------------------
+#include <bcrypt.h> 
+
